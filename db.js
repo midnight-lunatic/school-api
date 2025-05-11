0db45ca1a952baebc3dcf45ff536c875355dad4f
midnight lunatic <arohkartikeya@gmail.com>
2025-05-11 23:45:13 +0530

    ffs

--- db.js
+++ db.js
@@ -7,9 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: true // Aiven requires secure SSL connections
-  }
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
