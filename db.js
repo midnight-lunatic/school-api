5328b16cdd40e3c7511b395fc0b1f20a97231dc8
midnight lunatic <arohkartikeya@gmail.com>
2025-05-11 16:40:36 +0530

    Updated the databse to deploy on render

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const mysql = require('mysql');
+import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT || 3306
 });
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err.stack);
     return;
   }
-  console.log('Connected to Railway MySQL');
+  console.log('Connected to database.');
 });
+
+export default connection;
