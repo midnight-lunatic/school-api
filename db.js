b5c042d39093fca107598290f02d96f3bfb7496a
midnight lunatic <arohkartikeya@gmail.com>
2025-05-11 16:43:01 +0530

    Updated the databse to deploy on render

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
-import mysql from 'mysql2';
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Database connection failed:', err.stack);
+    console.error('Error connecting to MySQL:', err);
     return;
   }
-  console.log('Connected to database.');
+  console.log('Connected to MySQL database!');
 });
 
-export default connection;
+module.exports = connection;
